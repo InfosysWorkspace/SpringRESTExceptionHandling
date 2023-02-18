92656fd3d6d32c1387ba1ad1ae130fd94365f04f
BsoftMaewada <maewada@live.com>
2023-02-18 05:24:04 -0500

    completed error handling using @RestControllerAdvice

---
+++ src/main/java/com/infy/SpringRESTExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.infy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRESTExceptionHandling {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRESTExceptionHandling.class, args);
+	}
+
+}
